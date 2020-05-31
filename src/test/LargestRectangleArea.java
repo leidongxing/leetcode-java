73bd294810ee9a3afb6e470d53364e9b8c313b2b
leidongxing <leidongxing@gmail.com>
2020-05-31 21:13:02 +0800

    柱状图中最大的矩形

---
+++ src/test/LargestRectangleArea.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.util.Stack;
+
+/**
+ * @author LeiDongxing
+ * created on 2020/5/31
+ */
+public class LargestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        //从左往右  找出每一个位置的左边界 index
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = (stack.isEmpty() ? -1 : stack.peek());
+            stack.push(i);
+        }
+        stack.clear();
+        //从右向左 找到每一个位置的右边界 index
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = (stack.isEmpty() ? n : stack.peek());
+            stack.push(i);
+        }
+
+        //找所有位置的最大值
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
