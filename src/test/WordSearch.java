9b9e6e16b0033630548b5b8a336dba7c770d8c61
leidongxing <leidongxing@gmail.com>
2020-09-13 16:40:26 +0800

    单词搜索

--- src/test/WordSearch.java
+++ src/test/WordSearch.java
@@ -1,16 +1,53 @@
 package test;
 
+/**
+ * 单词搜索
+ */
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
-        char[] charWord = word.toCharArray();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == charWord[0]) {
-                    int k = charWord.length - 1;
+        int h = board.length;
+        int w = board[0].length;
+        //标记是否已经被使用  初始化为false
+        boolean[][] visited = new boolean[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                boolean flag = check(board, visited, i, j, word, 0);
+                if (flag) {
+                    return true;
                 }
             }
         }
-        return true;
+        return false;
+    }
+
+    private boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int start) {
+        //不等于 直接返回false
+        if (board[i][j] != s.charAt(start)) {
+            return false;
+        } else if (start == s.length() - 1) {
+            //等于 且已经到最后 返回true
+            return true;
+        }
+        visited[i][j] = true;
+        //定义四个方向
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        boolean result = false;
+        for (int[] dir : directions) {
+            int newi = i + dir[0];
+            int newj = j + dir[1];
+            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
+                if (!visited[newi][newj]) {
+                    boolean flag = check(board, visited, newi, newj, s, start + 1);
+                    if (flag) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        //还原
+        visited[i][j] = false;
+        return result;
     }
 
     public static void main(String[] args) {
