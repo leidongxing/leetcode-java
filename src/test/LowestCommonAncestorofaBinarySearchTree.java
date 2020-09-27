5941983bd4dacce0d33c3d4ec3715b59ae5950fc
leidongxing <leidongxing@gmail.com>
2020-09-27 23:42:28 +0800

    二叉搜索树的最近公共祖先

---
+++ src/test/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,18 @@
+package test;
+
+/**
+ * @author LeiDongxing
+ * created on 2020/9/27
+ * 二叉搜索树的最近公共祖先
+ */
+public class LowestCommonAncestorofaBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
