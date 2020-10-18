5689366dbe8e8d8fa4a827bb6d884438c2fb735c
leidongxing <leidongxing@gmail.com>
2020-10-18 17:59:41 +0800

    删除链表的倒数第N个节点

--- src/test/RemoveNthNodeFromEndofList.java
+++ src/test/RemoveNthNodeFromEndofList.java
@@ -3,9 +3,11 @@ package test;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * 删除链表的倒数第N个节点
+ */
 public class RemoveNthNodeFromEndofList {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
         Map<Integer, ListNode> map = new HashMap<Integer, ListNode>();
         int num = 0;
         map.put(num, head);
@@ -25,24 +27,25 @@ public class RemoveNthNodeFromEndofList {
         }
     }
 
-    //	public ListNode removeNthFromEnd(ListNode head, int n) {
-//	    ListNode start = new ListNode(0);
-//	    ListNode slow = start, fast = start;
-//	    slow.next = head;
-//	    
-//	    //Move fast in front so that the gap between slow and fast becomes n
-//	    for(int i=1; i<=n+1; i++)   {
-//	        fast = fast.next;
-//	    }
-//	    //Move fast to the end, maintaining the gap
-//	    while(fast != null) {
-//	        slow = slow.next;
-//	        fast = fast.next;
-//	    }
-//	    //Skip the desired node
-//	    slow.next = slow.next.next;
-//	    return start.next;
-//	}
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        //保存头节点
+        slow.next = head;
+
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //删除倒数第n个节点
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         ListNode next1 = new ListNode(2);
