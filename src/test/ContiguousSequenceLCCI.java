20244d014616a9d36248c0c5fe58af820a74cc88
leidongxing <leidongxing@gmail.com>
2021-01-28 21:58:05 +0800

    迭代版本

--- src/test/ContiguousSequenceLCCI.java
+++ src/test/ContiguousSequenceLCCI.java
@@ -6,7 +6,7 @@ package test;
  * 连续数列
  */
 public class ContiguousSequenceLCCI {
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray1(int[] nums) {
         int result = Integer.MIN_VALUE;
         if (nums.length == 0) {
             return result;
@@ -18,6 +18,22 @@ public class ContiguousSequenceLCCI {
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+
+
+    public int maxSubArray(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        if (nums.length == 0) {
+            return result;
+        }
+        //以i为结尾最大值
+        int pre = nums[0];
+        result = pre;
+        for (int i = 1; i < nums.length; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            result = Math.max(result, pre);
         }
         return result;
     }
