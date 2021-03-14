dac995442d11524bbcc2f9e28e26e24d116ee9b9
leidongxing <leidongxing@gmail.com>
2021-03-14 08:51:53 +0800

    设计哈希映射

---
+++ src/test/MyHashMap.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author LeiDongxing
+ * created on 2021/3/14
+ * 设计哈希映射
+ */
+public class MyHashMap {
+    private static class Pair {
+        private int key;
+        private int value;
+
+        public Pair(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+
+    private static final int BASE = 769;
+    private LinkedList<Pair>[] data;
+
+    public MyHashMap() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<>();
+        }
+    }
+
+    public void put(int key, int value) {
+        int h = hash(key);
+        Iterator<Pair> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                pair.setValue(value);
+                return;
+            }
+        }
+        data[h].offerLast(new Pair(key, value));
+    }
+
+    public int get(int key) {
+        int h = hash(key);
+        Iterator<Pair> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                return pair.value;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Pair> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                data[h].remove(pair);
+                return;
+            }
+        }
+    }
+
+    private int hash(int key) {
+        return key % BASE;
+    }
+}
