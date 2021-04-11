6c79dd7c9a8222516831b38b224cefa0f405342a
leidongxing <leidongxing@gmail.com>
2021-04-11 18:26:01 +0800

    丑数

--- src/test/UglyNumber.java
+++ src/test/UglyNumber.java
@@ -1,7 +1,13 @@
 package test;
 
+/**
+ * 丑数
+ * 丑数 只包含质因数 2、3 和/或 5 的正整数。
+ */
 public class UglyNumber {
-
+    /**
+     * 判断数是否为丑数
+     */
     public boolean isUgly(int num) {
         if (num == 1) {
             return true;
@@ -21,11 +27,57 @@ public class UglyNumber {
         return num == 1;
     }
 
+    /**
+     * 返回第n个丑数
+     */
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        for (int i = 2; i <= n; i++) {
+            int A = 2 * dp[a];
+            int B = 3 * dp[b];
+            int C = 5 * dp[c];
+            if (A < B && A < C) {
+                dp[i] = A;
+                a++;
+            } else if (B < A && B < C) {
+                dp[i] = B;
+                b++;
+            } else if (C < A && C < B) {
+                dp[i] = C;
+                c++;
+            } else if (A == B && B == C) {
+                dp[i] = A;
+                a++;
+                b++;
+                c++;
+            } else if (A == B) {
+                dp[i] = A;
+                a++;
+                b++;
+            } else if (A == C) {
+                dp[i] = A;
+                a++;
+                c++;
+            } else {
+                dp[i] = B;
+                b++;
+                c++;
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         UglyNumber un = new UglyNumber();
         // System.out.println(un.isUgly(14));
         // System.out.println(un.isUgly(1));
         // System.out.println(un.isUgly(7));
-        System.out.println(un.isUgly(10));
+        // System.out.println(un.isUgly(10));
+        System.out.println(un.nthUglyNumber(10));
     }
 }
