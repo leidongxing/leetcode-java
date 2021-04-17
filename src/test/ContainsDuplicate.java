db45d5b7423a0a2150e9064d59ce7bde6c75ff06
leidongxing <leidongxing@gmail.com>
2021-04-17 10:30:07 +0800

    存在重复元素 III

--- src/test/ContainsDuplicate.java
+++ src/test/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * @author LeiDongxing
@@ -15,6 +16,47 @@ public class ContainsDuplicate {
                 return true;
             }
             set.add(num);
+        }
+        return false;
+    }
+
+    /**
+     * 存在重复元素 III
+     * 存在 abs(nums[i] - nums[j]) <= t && abs(i - j) <= k
+     */
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        // nums[i]-t<=nums[j]
+        //维护大小为k的滑动窗口  遍历到元素x时  检查是否有元素落在[x-t,x+t]区间
+        //存储数组值
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long c = set.ceiling((long) nums[i] - (long) t);
+            //不能超出nums[i]+t
+            if (c != null && c <= (long) nums[i] + (long) t) {
+                return true;
+            }
+            set.add((long) nums[i]);
+            //移动滑动窗口
+            if (i >= k) {
+                set.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+
+
+    //暴力法超时
+    public boolean containsNearbyAlmostDuplicate1(int[] nums, int k, int t) {
+        if (nums.length == 0 || k <= 0 || t < 0) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= nums.length; j++) {
+                if (Math.abs(j - i) <= k && Math.abs((long) nums[j] - (long) nums[i]) <= t) {
+                    return true;
+                }
+            }
         }
         return false;
     }
