8fe87daa31945fd4da5b7290c43a7fba62f976d7
leidongxing <leidongxing@gmail.com>
2021-05-03 10:29:21 +0800

    整数反转

--- src/test/ReverseInteger.java
+++ src/test/ReverseInteger.java
@@ -1,16 +1,34 @@
 package test;
 
+/**
+ * 整数反转
+ */
 public class ReverseInteger implements Solution {
     public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result < Integer.MIN_VALUE / 10 || result > Integer.MAX_VALUE / 10) {
+                return 0;
+            } else {
+                int digit = x % 10;
+                x /= 10;
+                result = result * 10 + digit;
+            }
+        }
+        return result;
+    }
+
+    public int reverse1(int x) {
         long r = 0;
         while (x != 0) {
             r = r * 10 + x % 10;
             x /= 10;
         }
-        if (r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE)
+        if (r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE) {
             return (int) r;
-        else
+        } else {
             return 0;
+        }
     }
 
 
@@ -24,6 +42,6 @@ public class ReverseInteger implements Solution {
 
     public static void main(String[] args) {
         ReverseInteger r = new ReverseInteger();
-        new ClacTimeThread(r).run();
+        new ClacTimeThread(r).start();
     }
 }
