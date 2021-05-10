21b2351f57ffb92b16b78fb25f7130bfdf063604
leidongxing <leidongxing@gmail.com>
2021-05-10 22:19:57 +0800

    叶子相似的树

--- src/test/LeafSimilarTrees.java
+++ src/test/LeafSimilarTrees.java
@@ -1,16 +1,37 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 叶子相似的树
+ */
 public class LeafSimilarTrees {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        dfs(root1, list1);
+        dfs(root2, list2);
+        if (list1.size() != list2.size()) {
             return false;
         }
-
-
-        return false;
+        for (int i = 0; i < list1.size(); i++) {
+            if (!list1.get(i).equals(list2.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static void main(String[] args) {
-
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+        if (root.left != null) {
+            dfs(root.left, list);
+        }
+        if (root.right != null) {
+            dfs(root.right, list);
+        }
     }
 }
--- src/test/ListNode.java
+++ src/test/ListNode.java
@@ -9,6 +9,11 @@ public class ListNode {
         val = x;
     }
 
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
     @Override
     public String toString() {
         String str = "index:" + val;
