2ef0f87aecf354a371f94c6c8fb43046c4039065
leidongxing <leidongxing@gmail.com>
2021-06-07 22:57:03 +0800

    三角形的最大周长

--- src/test/LargestPerimeterTriangle.java
+++ src/test/LargestPerimeterTriangle.java
@@ -16,50 +16,6 @@ public class LargestPerimeterTriangle {
         return 0;
     }
 
-
-    public int largestPerimeter2(int[] A) {
-        quickSort(A, 0, A.length - 1);
-        int i = 0, j = 1, k = 2;
-        while (i < A.length - 2) {
-            while (k < A.length) {
-                if (A[i] < A[j] + A[k]) {
-                    return A[i] + A[j] + A[k];
-                }
-                k++;
-            }
-            i++;
-            j++;
-            k = j + 1;
-        }
-        return 0;
-    }
-
-    int[] quickSort(int[] A, int left, int right) {
-        int p = 0;
-        if (left < right) {
-            p = partition(A, left, right);
-            quickSort(A, left, p - 1);
-            quickSort(A, p + 1, right);
-        }
-        return A;
-    }
-
-    int partition(int[] A, int left, int right) {
-        int key = A[left];
-        while (left < right) {
-            while (left < right && A[right] <= key) {
-                right--;
-            }
-            A[left] = A[right];
-            while (left < right && A[left] >= key) {
-                left++;
-            }
-            A[right] = A[left];
-        }
-        A[left] = key;
-        return left;
-    }
-
     public int largestPerimeter1(int[] A) {
         Arrays.sort(A);
         int i = A.length - 1, j = A.length - 2, k = A.length - 3;
