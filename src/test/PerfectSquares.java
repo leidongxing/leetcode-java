568f1e6e09062b1b8c3bb6cda7a616b5627eced4
leidongxing <leidongxing@gmail.com>
2021-06-11 23:21:12 +0800

    完全平方数

---
+++ src/test/PerfectSquares.java
@@ -0,0 +1,24 @@
+package test;
+
+/**
+ * @author LeiDongxing
+ * created on 2021/6/11
+ * 完全平方数
+ */
+public class PerfectSquares {
+    /**
+     * @param n 返回和为n的完全平方数的最少数量
+     */
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
