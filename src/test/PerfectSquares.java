43fa95e2da1db7acdb27d2922c18c4f561357b49
leidongxing <leidongxing@gmail.com>
2021-06-11 23:23:24 +0800

    完全平方数

--- src/test/PerfectSquares.java
+++ src/test/PerfectSquares.java
@@ -11,9 +11,10 @@ public class PerfectSquares {
      */
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = 1;
+        dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
+            //最大就是 由1拼成
             dp[i] = i;
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
