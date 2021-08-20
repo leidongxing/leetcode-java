b2dc6bf0a7e6f2d5439bff171d2b38cad62e2f9e
leidongxing <leidongxing@qq.com>
2021-08-20 23:05:27 +0800

    反转字符串

--- src/test/ReverseString.java
+++ src/test/ReverseString.java
@@ -1,5 +1,8 @@
 package test;
 
+/**
+ * 反转字符串
+ */
 public class ReverseString {
     public String reverseString(String s) {
         char[] chars = s.toCharArray();
@@ -20,9 +23,34 @@ public class ReverseString {
         }
     }
 
+    /**
+     * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+     * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+     */
+    public String reverseStr(String s, int k) {
+        char[] array = s.toCharArray();
+        for (int i = 0; i < s.length(); i += 2 * k) {
+            reverse(array, i, Math.min(i + k, s.length()) - 1);
+        }
+        return new String(array);
+    }
+
+    public void reverse(char[] array, int left, int right) {
+        while (left < right) {
+            char tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
     public static void main(String[] args) {
         ReverseString rs = new ReverseString();
         System.out.println(rs.reverseString("hello"));
         System.out.println(rs.reverseString("A man, a plan, a canal: Panama"));
+        System.out.println(rs.reverseStr("abcdefg", 2)); //bacdfeg
+        System.out.println(rs.reverseStr("abcd", 2));//bacd
+        System.out.println(rs.reverseStr("abcdefg", 8));//gfedcba
     }
 }
