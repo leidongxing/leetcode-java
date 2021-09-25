c578f4eae63e4a02ca9d5b4bdc4dbb675bec601e
leidongxing <leidongxing@qq.com>
2021-09-25 12:20:29 +0800

    两个字符串的删除操作

---
+++ src/test/DeleteOperationForTwoStrings.java
@@ -0,0 +1,35 @@
+package test;
+
+/**
+ * @author LeiDongxing
+ * created on 2021/9/25
+ * 两个字符串的删除操作
+ */
+public class DeleteOperationForTwoStrings {
+    /**
+     * @return 找到使得word1和word2相同所需的最小步数，每步可以删除任意一个字符串中的一个字符
+     */
+    public int minDistance(String word1, String word2) {
+        //找出两个字符串的最长公共子序列
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - 2 * dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        DeleteOperationForTwoStrings d = new DeleteOperationForTwoStrings();
+        System.out.println(d.minDistance("sea", "eat"));//ea ea 2
+    }
+}
