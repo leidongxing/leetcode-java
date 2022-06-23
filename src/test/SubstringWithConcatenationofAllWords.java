020a0da8e7c4d35cdc95aec2d0fb12b11bb61317
leidongxing <leidongxing@qq.com>
2022-06-23 23:04:04 +0800

    串联所有单词的子串

--- src/test/SubstringWithConcatenationofAllWords.java
+++ src/test/SubstringWithConcatenationofAllWords.java
@@ -1,17 +1,36 @@
 package test;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
+/**
+ * 串联所有单词的子串
+ */
 public class SubstringWithConcatenationofAllWords {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> al = new LinkedList<Integer>();
-        List<String> wordsList = new LinkedList<String>();
-        for (int i = 0; i < words.length; i++) {
-            wordsList.add(words[i]);
+        int n = s.length();
+        int m = words.length;
+        int w = words[0].length();
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
-
-        return al;
+        List<Integer> result = new ArrayList<>();
+        out:
+        for (int i = 0; i + m * w <= n; i++) {
+            Map<String, Integer> cur = new HashMap<>();
+            String sub = s.substring(i, i + m * w);
+            for (int j = 0; j < sub.length(); j += w) {
+                String item = sub.substring(j, j + w);
+                if (!map.containsKey(item)) {
+                    continue out;
+                }
+                cur.put(item, cur.getOrDefault(item, 0) + 1);
+            }
+            if (cur.equals(map)) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
